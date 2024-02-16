2140f29c7cbb9640f6f3dbfd7a036adf54d69faa
CyberGhost04 <sidhantbahuguna@gmail.com>
2024-02-16 14:58:41 +0530

    Still working

--- src/App.jsx
+++ src/App.jsx
@@ -14,16 +14,16 @@ function App() {
 
   const [background, setbackground] = useState("bg-gradient-to-r from-black to-gray-500")
 
-  const EditTask = ()=>{}
+  const EditTask = () => { }
 
-  const DelTask = ()=>{}
+  const DelTask = () => { }
 
-  const AddTask =()=>{
-    settasks([...tasks, {id: uuidv4(), val}])
+  const AddTask = () => {
+    settasks([...tasks, { id: uuidv4(), val }])
     console.log(tasks)
   }
 
-  const ValChanged = (e)=>{
+  const ValChanged = (e) => {
     setval(e.target.value)
   }
 
@@ -31,9 +31,9 @@ function App() {
     <>
       <div className={`nav h-[20vh] w-[100%] flex align-middle justify-center relative ${background}`}>
         <div className="circle absolute right-0 flex gap-6 my-14 mx-16">
-          <button className='border-[3px] border-solid border-black w-12 h-12 rounded-[50%] bg-gradient-to-r from-gray-800 to-gray-300' onClick={() => { setbackground("bg-gradient-to-r from-gray-800 to-gray-300") }}></button>
-          <button className='border-[3px] border-solid border-black w-12 h-12 rounded-[50%] bg-gradient-to-r from-gray-900 to-gray-400' onClick={() => { setbackground("bg-gradient-to-r from-gray-900 to-gray-400") }}></button>
-          <button className='border-[3px] border-solid border-black w-12 h-12 rounded-[50%] bg-gradient-to-r from-black to-gray-500' onClick={() => { setbackground("bg-gradient-to-r from-black to-gray-500") }}></button>
+          <button className='border-[3px] border-solid border-black w-12 h-12 rounded-[50%] bg-gradient-to-r from-gray-800 to-gray-300 cursor-pointer' onClick={() => { setbackground("bg-gradient-to-r from-gray-800 to-gray-300") }}></button>
+          <button className='border-[3px] border-solid border-black w-12 h-12 rounded-[50%] bg-gradient-to-r from-gray-900 to-gray-400 cursor-pointer' onClick={() => { setbackground("bg-gradient-to-r from-gray-900 to-gray-400") }}></button>
+          <button className='border-[3px] border-solid border-black w-12 h-12 rounded-[50%] bg-gradient-to-r from-black to-gray-500 cursor-pointer' onClick={() => { setbackground("bg-gradient-to-r from-black to-gray-500") }}></button>
         </div>
       </div>
 
@@ -43,31 +43,22 @@ function App() {
         </div>
         <div className="input flex justify-center align-middle">
           <input type="text" id='task' className='w-[37%] h-[43px] rounded-l-[50px] bg-[#222222] text-white outline-none pl-6' value={val} onChange={ValChanged} />
-          <span className='w-20 h-[43px] text-black bg-white font-bold rounded-r-[50px] border-solid border-black border-2 flex flex-col align-middle justify-around pl-2' onClick={AddTask}>Lets Go</span>
+          <span className='w-20 h-[43px] text-black bg-white font-bold rounded-r-[50px] border-solid border-black border-2 flex flex-col align-middle justify-around pl-2 cursor-pointer' onClick={AddTask}>Lets Go</span>
         </div>
       </div>
       <DateTime req={background} />
       <div className={`p-4 nav h-[41vh] w-[100%] flex align-middle justify-center ${background}`}>
         <div className="flex flex-col align-top gap-2 overflow-auto">
-          <div className="text-white info flex justify-between bg-[#353839] border-[#353839] rounded-[50px] border-2 border-solid p-4 max-h-[4rem] max-w-[23rem] min-w-[23rem] ">
-            <p className='pl-2'>Tasks goes here</p>
-            <div className="btns flex gap-4">
-              <button className='invert'><img src={Pencil} alt="" /></button>
-              <button className='invert'><img src={Check} alt="" /></button>
-            </div>
-          </div>
 
-          {tasks.map((items)=>{
-            return 
+          {tasks.map((items) => {
+            return <div key={items.id} className="text-white info flex justify-between bg-[#353839] border-[#353839] rounded-[50px] border-2 border-solid p-4 max-h-[40rem] min-w-[23rem] ">
+              <p className='pl-2 overflow-y-auto overflow-x-hidden break-all max-w-[16rem] max-h-[20rem]'>{items.val}</p>
+              <div className="btns flex gap-4">
+                <button className='invert min-h-2 min-w-2 cursor-pointer' onClick={EditTask}><img src={Pencil} alt="" /></button>
+                <button className='invert min-h-2 min-w-2 cursor-pointer' onClick={DelTask}><img src={Check} alt="" /></button>
+              </div>
+            </div>
           })}
-
-          <div className="text-white info flex justify-between bg-[#353839] border-[#353839] rounded-[50px] border-2 border-solid p-4 max-h-[4rem] max-w-[23rem] min-w-[23rem] ">
-            <p className='pl-2'>Tasks goes here</p>
-            <div className="btns flex gap-4">
-              <button className='invert' onClick={EditTask}><img src={Pencil} alt="" /></button>
-              <button className='invert' onClick={DelTask}><img src={Check} alt="" /></button>
-            </div>
-          </div>
 
         </div>
       </div>
