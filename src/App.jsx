d4ca40c5e8b1d8541338cc09bef8c2ec5b04aa6f
CyberGhost04 <sidhantbahuguna@gmail.com>
2024-02-17 22:01:39 +0530

    More features incoming

--- src/App.jsx
+++ src/App.jsx
@@ -16,11 +16,19 @@ function App() {
 
   const EditTask = () => { }
 
-  const DelTask = () => { }
+  const DelTask = (e, id) => {
+    let newtasks = tasks.filter((item) => {
+      return item.id!=id
+    })
+
+    settasks(newtasks)
+
+  }
 
   const AddTask = () => {
     settasks([...tasks, { id: uuidv4(), val }])
     console.log(tasks)
+    setval("")
   }
 
   const ValChanged = (e) => {
@@ -42,12 +50,13 @@ function App() {
           <h1 className='text-9xl text-white'>Just Do It.|</h1>
         </div>
         <div className="input flex justify-center align-middle">
-          <input type="text" id='task' className='w-[37%] h-[43px] rounded-l-[50px] bg-[#222222] text-white outline-none pl-6' value={val} onChange={ValChanged} />
-          <span className='w-20 h-[43px] text-black bg-white font-bold rounded-r-[50px] border-solid border-black border-2 flex flex-col align-middle justify-around pl-2 cursor-pointer' onClick={AddTask}>Lets Go</span>
+          <input type="text" placeholder='Now or Never' id='task' className='w-[37%] h-[43px] rounded-l-[50px] bg-[#222222] text-white outline-none pl-6' value={val} onChange={ValChanged} />
+          <button disabled={val.length<1} className='w-20 h-[43px] text-black bg-white font-bold rounded-r-[50px] border-solid border-black border-2 flex flex-col align-middle justify-around pl-2 cursor-pointer disabled:bg-[#222222] disabled:border-[#222222] disabled:text-[#222222]' onClick={AddTask}>Lets Go</button>
         </div>
       </div>
       <DateTime req={background} />
-      <div className={`p-4 nav h-[41vh] w-[100%] flex align-middle justify-center ${background}`}>
+      {tasks.length===0 && <div className={`p-4 flex justify-center ${background} text-[17px] text-white`}>Consistency will always beat Talent</div>}
+      <div className={`p-4 nav h-[42vh] w-[100%] flex align-middle justify-center ${background}`}>
         <div className="flex flex-col align-top gap-2 overflow-auto">
 
           {tasks.map((items) => {
@@ -55,7 +64,7 @@ function App() {
               <p className='pl-2 overflow-y-auto overflow-x-hidden break-all max-w-[16rem] max-h-[20rem]'>{items.val}</p>
               <div className="btns flex gap-4">
                 <button className='invert min-h-2 min-w-2 cursor-pointer' onClick={EditTask}><img src={Pencil} alt="" /></button>
-                <button className='invert min-h-2 min-w-2 cursor-pointer' onClick={DelTask}><img src={Check} alt="" /></button>
+                <button className='invert min-h-2 min-w-2 cursor-pointer' onClick={(e)=>{DelTask(e, items.id)}}><img src={Check} alt="" /></button>
               </div>
             </div>
           })}
