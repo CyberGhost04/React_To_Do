10fca9955a16106d9a04a22acd6cb266ea7cfe1f
CyberGhost04 <sidhantbahuguna@gmail.com>
2024-02-17 22:58:22 +0530

    Two features left

--- src/App.jsx
+++ src/App.jsx
@@ -14,11 +14,20 @@ function App() {
 
   const [background, setbackground] = useState("bg-gradient-to-r from-black to-gray-500")
 
-  const EditTask = () => { }
+  const EditTask = (e, id) => {
+    e.stopPropagation()
+    tasks.filter((item) => {
+      if (item.id === id) {
+        setval(item.val)
+      }
+    })
+    DelTask(e, id)
+  }
 
   const DelTask = (e, id) => {
+    e.stopPropagation()                       // stops parent on click method
     let newtasks = tasks.filter((item) => {
-      return item.id!=id
+      return item.id != id
     })
 
     settasks(newtasks)
@@ -26,13 +35,26 @@ function App() {
   }
 
   const AddTask = () => {
-    settasks([...tasks, { id: uuidv4(), val }])
-    console.log(tasks)
+    settasks([...tasks, { id: uuidv4(), val, isComplete: false }])
     setval("")
   }
 
   const ValChanged = (e) => {
     setval(e.target.value)
+  }
+
+  const checkbox = (e, id) => {
+    let eleid = id
+    let index = tasks.findIndex((item) => {
+      return item.id === eleid
+    })
+    let newTasks = [...tasks]
+    newTasks[index].isComplete = !newTasks[index].isComplete
+    settasks(newTasks)
+  }
+
+  function childFunction(event) {
+    event.stopPropagation()
   }
 
   return (
@@ -51,25 +73,27 @@ function App() {
         </div>
         <div className="input flex justify-center align-middle">
           <input type="text" placeholder='Now or Never' id='task' className='w-[37%] h-[43px] rounded-l-[50px] bg-[#222222] text-white outline-none pl-6' value={val} onChange={ValChanged} />
-          <button disabled={val.length<1} className='w-20 h-[43px] text-black bg-white font-bold rounded-r-[50px] border-solid border-black border-2 flex flex-col align-middle justify-around pl-2 cursor-pointer disabled:bg-[#222222] disabled:border-[#222222] disabled:text-[#222222]' onClick={AddTask}>Lets Go</button>
+          <button disabled={val.length < 1} className='w-20 h-[43px] text-black bg-white font-bold rounded-r-[50px] border-solid border-black border-2 flex flex-col align-middle justify-around pl-2 cursor-pointer disabled:bg-[#222222] disabled:border-[#222222] disabled:text-[#222222] disabled:cursor-default' onClick={AddTask}>Lets Go</button>
         </div>
       </div>
       <DateTime req={background} />
-      {tasks.length===0 && <div className={`p-4 flex justify-center ${background} text-[17px] text-white`}>Consistency will always beat Talent</div>}
-      <div className={`p-4 nav h-[42vh] w-[100%] flex align-middle justify-center ${background}`}>
+      {tasks.length === 0 && <div className={`p-4 flex justify-center ${background} text-[17px] text-white`}>Consistency will always beat Talent</div>}
+      <div className={`p-4 nav h-[38vh] w-[100%] flex align-middle justify-center ${background}`}>
         <div className="flex flex-col align-top gap-2 overflow-auto">
-
           {tasks.map((items) => {
-            return <div key={items.id} className="text-white info flex justify-between bg-[#353839] border-[#353839] rounded-[50px] border-2 border-solid p-4 max-h-[40rem] min-w-[23rem] ">
-              <p className='pl-2 overflow-y-auto overflow-x-hidden break-all max-w-[16rem] max-h-[20rem]'>{items.val}</p>
-              <div className="btns flex gap-4">
-                <button className='invert min-h-2 min-w-2 cursor-pointer' onClick={EditTask}><img src={Pencil} alt="" /></button>
-                <button className='invert min-h-2 min-w-2 cursor-pointer' onClick={(e)=>{DelTask(e, items.id)}}><img src={Check} alt="" /></button>
+            return <div key={items.id} onClick={(e) => { checkbox(e, items.id) }} className="text-white info flex justify-between bg-[#353839] border-[#353839] rounded-[50px] border-2 border-solid p-4 max-h-[40rem] min-w-[23rem] cursor-vertical-text">
+              <p className={`overflow-y-auto overflow-x-hidden break-all max-w-[16rem] max-h-[20rem] pl-2 ${items.isComplete ? "line-through" : ""}`}>{items.val}</p>
+              <div className="btns flex gap-4" onClick={childFunction(event)}>
+                <button className='invert min-h-2 min-w-2 cursor-pointer' onClick={(e) => { EditTask(e, items.id) }}><img src={Pencil} alt="" /></button>
+                <button className='invert min-h-2 min-w-2 cursor-pointer' onClick={(e) => { DelTask(e, items.id) }}><img src={Check} alt="" /></button>
               </div>
             </div>
           })}
 
         </div>
+      </div>
+      <div className={`nav h-[4vh] w-[100%] flex align-middle justify-center ${background}`}>
+        <input type="checkbox" />
       </div>
     </>
   )
